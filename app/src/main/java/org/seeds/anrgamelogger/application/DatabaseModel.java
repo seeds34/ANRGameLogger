b7f9d57eac3543401f1be0ab9ac9a2388ddd9434
seeds34 <seeds34@hotmail.com>
2018-02-28 21:12:07 +0000

    Working on DBModel (28/02/2018)

--- app/src/main/java/org/seeds/anrgamelogger/application/DatabaseModel.java
+++ app/src/main/java/org/seeds/anrgamelogger/application/DatabaseModel.java
@@ -1,29 +1,16 @@
 package org.seeds.anrgamelogger.application;
 
-import static android.R.attr.version;
-
 import android.database.Cursor;
-import android.database.DatabaseUtils;
-import android.location.Location;
 import android.net.Uri;
-import android.util.Log;
 import com.pushtorefresh.storio3.contentresolver.StorIOContentResolver;
 import com.pushtorefresh.storio3.contentresolver.queries.Query;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import okhttp3.OkHttpClient;
 import org.seeds.anrgamelogger.database.contracts.IdentitiesContract;
 import org.seeds.anrgamelogger.database.contracts.LocationsContract;
 import org.seeds.anrgamelogger.database.contracts.LoggedGamesContract;
 import org.seeds.anrgamelogger.database.contracts.PlayersContract;
-import org.seeds.anrgamelogger.model.Game;
 import org.seeds.anrgamelogger.model.Identity;
-import org.seeds.anrgamelogger.model.Player;
-
 import retrofit2.Retrofit;
 
 /**
@@ -36,31 +23,37 @@ public class DatabaseModel {
   private StorIOContentResolver storIOContentResolver;
   private OkHttpClient okHttpClient;
   private Retrofit retrofit;
-  private HashMap<Uri, Class> tableToClassMap;
+  //private HashMap<Uri, Class> tableToClassMap;
 
   public DatabaseModel(StorIOContentResolver storIOContentResolverIn, OkHttpClient okHttpClientIn, Retrofit retrofitIn){
     storIOContentResolver = storIOContentResolverIn;
     okHttpClient = okHttpClientIn;
     retrofit = retrofitIn;
-    databaseToClassModel();
+//    databaseToClassModel();
   }
 
-  public void databaseToClassModel(){
-
-    tableToClassMap = new HashMap();
-    tableToClassMap.put(IdentitiesContract.URI_TABLE, Identity.class);
-    //tableToClassMap.put(LocationsContract.URI_TABLE, )
-    tableToClassMap.put(LoggedGamesContract.URI_TABLE, Game.class);
-    tableToClassMap.put(PlayersContract.URI_TABLE, Player.class);
+  public boolean isIdentitiesTableEmpty(){
+    return isTableEmpty(IdentitiesContract.URI_TABLE);
   }
 
-  public boolean isTableEmpty(Uri tableUri){
+  public boolean isLocationTableEmpty(){
+    return isTableEmpty(LocationsContract.URI_TABLE);
+  }
+
+  public boolean isPlayerTableEmpty(){
+    return isTableEmpty(PlayersContract.URI_TABLE);
+  }
+
+  public boolean isLoggedgamesTableEmpty(){
+    return isTableEmpty(LoggedGamesContract.URI_TABLE);
+  }
+  private boolean isTableEmpty(Uri tableUri){
 
     boolean ret = false;
 
     Cursor queryResult = storIOContentResolver
         .get()
-            .cursor()
+        .cursor()
         .withQuery(Query.builder()
             .uri(tableUri)
             .build())
@@ -74,33 +67,72 @@ public class DatabaseModel {
     return ret;
   }
 
-  private boolean find(Uri tableUri, String coloumnName, String valueIn){
+  private List<Identity> getIdentities(){
 
-    boolean ret = false;
-    String value = DatabaseUtils.sqlEscapeString(valueIn);
-    String whereClause = coloumnName + " = " + value ;
-    Class typeClass = tableToClassMap.get(tableUri);
+    return storIOContentResolver
+        .get()
+        .listOfObjects(Identity.class)
+        .withQuery(Query.builder()
+            .uri(IdentitiesContract.URI_TABLE)
+            .build())
+        .prepare()
+        .executeAsBlocking();
 
-
-      ArrayList<?> queryResult = storIOContentResolver
-            .get()
-            .listOfObjects(typeClass.getClass())
-            .withQuery(Query.builder()
-                    .uri(tableUri)
-                    .whereArgs(coloumnName + " = ?")
-                    .where(value)
-                    .build())
-            .prepare()
-            .executeAsBlocking();
-
-    if(queryResult != null && queryResult.getCount() > 0){
-      ret = true;
-    }
-
-    Log.d(LOG_TAG, "Looking for: D: " + coloumnName + " I: " + valueIn + " V: " + version + " Returned: " + ret);
-
-    return ret;
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Genric soultion ideas
+
+//  public void databaseToClassModel(){
+//
+//    tableToClassMap = new HashMap();
+//    tableToClassMap.put(IdentitiesContract.URI_TABLE, Identity.class);
+//    //tableToClassMap.put(LocationsContract.URI_TABLE, )
+//    tableToClassMap.put(LoggedGamesContract.URI_TABLE, Game.class);
+//    tableToClassMap.put(PlayersContract.URI_TABLE, Player.class);
+//  }
+
+
+
+//  private boolean find(Uri tableUri, String coloumnName, String valueIn){
+//
+//    boolean ret = false;
+//    String value = DatabaseUtils.sqlEscapeString(valueIn);
+//    String whereClause = coloumnName + " = " + value ;
+//    Class typeClass = tableToClassMap.get(tableUri);
+//
+//
+//      ArrayList<?> queryResult = storIOContentResolver
+//            .get()
+//            .listOfObjects(typeClass.getClass())
+//            .withQuery(Query.builder()
+//                    .uri(tableUri)
+//                    .whereArgs(coloumnName + " = ?")
+//                    .where(value)
+//                    .build())
+//            .prepare()
+//            .executeAsBlocking();
+//
+//    if(queryResult != null && queryResult.getCount() > 0){
+//      ret = true;
+//    }
+//
+//    Log.d(LOG_TAG, "Looking for: D: " + coloumnName + " I: " + valueIn + " V: " + version + " Returned: " + ret);
+//
+//    return ret;
+//  }
 
 
 }
