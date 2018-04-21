b9a71c36af48bd56fefdbe9eaa1f428ef9aa266c
seeds34 <seeds34@hotmail.com>
2018-04-21 20:51:19 +0100

    Working on Overview PArt (21/04)

--- app/src/main/java/org/seeds/anrgamelogger/addgame/views/AddGameOverview.java
+++ app/src/main/java/org/seeds/anrgamelogger/addgame/views/AddGameOverview.java
@@ -1,7 +1,14 @@
 package org.seeds.anrgamelogger.addgame.views;
 
 import android.app.Activity;
+import android.widget.Button;
+
+import com.jakewharton.rxbinding2.view.RxView;
+
 import org.seeds.anrgamelogger.R;
+
+import butterknife.BindView;
+import io.reactivex.Observable;
 
 /**
  * Created by Tomas Seymour-Turner on 04/01/2018.
@@ -13,6 +20,9 @@ public class AddGameOverview extends AddGameBaseView{
   //TODO: Add common type between ADDGame Views??
   private final String TITLE = "Overview";
   private Activity activity;
+
+  @BindView(R.id.btn_save)
+  public Button btn_save;
 
   public AddGameOverview(Activity activity){
     super(activity);
@@ -26,4 +36,7 @@ public class AddGameOverview extends AddGameBaseView{
     return TITLE;
   }
 
+  public Observable<Object> save(){
+    return RxView.clicks(btn_save);
+  }
 }
